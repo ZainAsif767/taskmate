54677757b00fff04c1b39f3b8cc4509276d87edc
zainasif767 <zainclasher2002@gmail.com>
2023-02-05 00:20:45 +0500

    Fix

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,10 +1,29 @@
 import React from "react";
 
-export default function ShowTask({ taskList, setTaskList }) {
+export default function ShowTask({ taskList, setTaskList, updateTask, setUpdateTask }) {
 
     function handleClear() {
         setTaskList([])
     }
+    // function handleEdit(id) {
+
+    // }
+    // function handleDelete(id) {
+    //     const updatedTaskList = taskList.filter(todo => todo.id !== id)
+    //     setTaskList(updatedTaskList)
+
+    // }
+
+    const taskElements = taskList.map(todo =>
+        <li key={todo.id}>
+            <p>
+                <span className="name">{todo.name}</span>
+                <span className="time">{todo.time}</span>
+            </p>
+            <i className="bi bi-pencil-square"></i>
+            <i className="bi bi-trash"></i>
+        </li>
+    )
 
     return (
         <section className="showTask">
@@ -16,16 +35,7 @@ export default function ShowTask({ taskList, setTaskList }) {
                 <button className="clearAll" onClick={handleClear}>Clear All</button>
             </div>
             <ul>
-                {taskList && taskList.map((task) => (
-                    <li key={task.id}>
-                        <p>
-                            <span className="name">{task.name}</span>
-                            <span className="time">{task.time}</span>
-                        </p>
-                        <i className="bi bi-pencil-square"></i>
-                        <i className="bi bi-trash"></i>
-                    </li>
-                ))}
+                {taskElements}
             </ul>
         </section>
     )
