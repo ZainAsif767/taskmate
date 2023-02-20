fe07c2ef4b6e8194e60a25100a417dcced5c2f5f
zainasif767 <zainclasher2002@gmail.com>
2023-02-21 00:13:57 +0500

    Feat: Localstorage for notes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import AddTask from './components/AddTask';
 import ShowTask from './components/ShowTask';
 import './App.css';
 import { useState } from 'react';
+import React from 'react';
 
 export default function App() {
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState(JSON.parse(localStorage.getItem("tasklist")) || []);
   const [updateTask, setUpdateTask] = useState({});
+
+  React.useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(taskList))
+  }, [taskList])
 
   return (
     <div>
