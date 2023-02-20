fd16c4459a7943a3221d723bb3174be693dfb29e
zainasif767 <zainclasher2002@gmail.com>
2023-02-21 00:14:17 +0500

    Feat: theme Selection

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import Logo from "../assets/logo.png"
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Header() {
+    const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("theme")) || "medium")
+
+    useEffect(() => {
+        localStorage.setItem("theme", JSON.stringify(theme))
+        document.documentElement.removeAttribute("class")
+        document.documentElement.classList.add(theme)
+    }, [theme])
     return (
         <header>
             <div className="logo">
@@ -9,12 +16,12 @@ export default function Header() {
                 <span>TaskMate</span>
             </div>
             <div className="themeSelector">
-                <span className="light"></span>
-                <span className="medium activeTheme"></span>
-                <span className="dark"></span>
-                <span className="gradientone"></span>
-                <span className="gradienttwo"></span>
-                <span className="gradientthree"></span>
+                <span onClick={() => setTheme("light")} className={theme === "light" ? "light activeTheme" : "light"}></span>
+                <span onClick={() => setTheme("dark")} className={theme === "dark" ? "dark activeTheme" : "dark"}></span>
+                <span onClick={() => setTheme("medium")} className={theme === "medium" ? "medium activeTheme" : "medium"}></span>
+                <span onClick={() => setTheme("gradientone")} className={theme === "gradientone" ? "gradientone activeTheme" : "gradientone"}></span>
+                <span onClick={() => setTheme("gradienttwo")} className={theme === "gradienttwo" ? "gradienttwo activeTheme" : "gradienttwo"}></span>
+                <span onClick={() => setTheme("gradientthree")} className={theme === "gradientthree" ? "gradientthree activeTheme" : "gradientthree"}></span>
             </div>
         </header>
     )
